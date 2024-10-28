0e37d2adc163e43db9229c155cb0dd34d3c3fc27
ArpitaKaushal <arpitakaushal538@gmail.com>
2024-10-28 18:23:44 +0530

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursedetailsComponent } from './pages/coursedetails/coursedetails.component';
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
 
-export const routes: Routes = [];
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"home",
+        pathMatch:"full"
+    },
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"coursedetails",
+        component:CoursedetailsComponent
+    },
+    
+    {
+        path:"my-courses",
+        component:MyCoursesComponent
+    },
+    { path: "register", 
+        component: RegisterComponent },
+
+    { path: "login",
+         component: LoginComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
